4259a1fb98ef6024e1dd1e9497d9700ca1169bb5
pdavis327 <petedavis327@gmail.com>
2025-06-27 16:07:55 -0400

    add minio resource yaml

--- K8s/auth/htpass-create.sh
+++ K8s/auth/htpass-create.sh
@@ -2,7 +2,7 @@
 
 # Create htpasswd file for demo users
 htpasswd -c -B -b users.htpasswd demo-user demo123
-htpasswd -B -b users.htpasswd admin-user admin123
-htpasswd -B -b users.htpasswd arrow-user arrow123
+htpasswd -b users.htpasswd admin-user admin123
+htpasswd -b users.htpasswd arrow-user arrow123
 
 echo "Created htpasswd file with demo users"
